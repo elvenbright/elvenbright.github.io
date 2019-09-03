52f1d5bc82ebcfc347eaa92c5132dde219e28ba5
danilads <vfhn3fghtkm>
2019-09-03 17:20:43 +0300

    updt

--- components/Block_MainPage.js
+++ components/Block_MainPage.js
@@ -1,56 +1,22 @@
 import React, { Fragment } from 'react';
 import { Icon } from 'antd';
+import OpacityAndBlockTest from './OpacityAndBlockTest';
 
 
 
 class Block_MainPage extends React.PureComponent {
-	state = {
-		defaultText: 'some long text some long text some long text some long text some long text some long text some long text'
-	}
-	ref = React.createRef();
 
-	myResize = () => {
-	
-		let block = this.ref.current;
-		let text = this.state.defaultText;
-		console.log('---works1?',block);
-		console.log('---works2?',text);
-		block.innerHTML = text;
-
-
-
-		if (block.scrollHeight - block.offsetHeight > 1) {
-			const wordArray = text.split(' ');
-			let deletedWord = '';
-			while (block.scrollHeight - block.offsetHeight > 1) {
-				deletedWord = wordArray.pop();
-				block.innerHTML = wordArray.join(' ');
-			}
-			wordArray.push(deletedWord);
-			let txt1 = wordArray.join(' ');
-			const textArray = [...text];
-			block.innerHTML = txt1;
-			while (block.scrollHeight - block.offsetHeight > 1) {
-				textArray.pop();
-				txt1 = textArray.join('');
-				block.innerHTML = txt1;
-			}
-			const gra = textArray.splice(textArray.length - 5, 5).map((s, i, a) => `<span style='opacity: 0.${1 + (100 * (8 * (1 - (i / (a.length - 1)))))};'>${s}</span>`);
-			txt1 = textArray.join('') + gra.join('');
-			block.innerHTML = txt1;
-		}
-	};
-
-	componentDidMount() {
-		window.addEventListener('resize',this.myResize);
-		this.myResize();
-	};
 
 	render() {
 		return (
 			<div>
-				<div>font Ie test</div>
-				<div style={{lineHeight:'1em',maxHeight:'1em',wordBreak:'break-all'}} ref={this.ref} className={"Block_Opacity"}></div>
+                <div>Readme:(тестировать на windows - chrome) 1 - должна работать opacity при ресайзе  2 - шрифт должен идти по низу блока</div>
+                <hr/><br/>
+                <div>нормальный шрифт</div>
+                <OpacityAndBlockTest font="googleFont"/>
+
+                <div>плохой шрифт шрифт</div>
+                <OpacityAndBlockTest font="SeroPro"/>
 				<div><Icon type="step-backward"/></div>
 			</div>
 		);
---
+++ components/OpacityAndBlockTest.js
@@ -0,0 +1,65 @@
+import React from 'react';
+
+
+
+
+class OpacityAndBlockTest extends React.PureComponent {
+	state = {
+		defaultText: 'some long text some long text some long text some long text some long text some long text some long text'
+	}
+	ref = React.createRef();
+
+	myResize = () => {
+	
+		let block = this.ref.current;
+		let text = this.state.defaultText;
+	
+		block.innerHTML = text;
+
+
+
+		if (block.scrollHeight - block.offsetHeight > 1) {
+			const wordArray = text.split(' ');
+			let deletedWord = '';
+			while (block.scrollHeight - block.offsetHeight > 1) {
+				deletedWord = wordArray.pop();
+				block.innerHTML = wordArray.join(' ');
+			}
+			wordArray.push(deletedWord);
+			let txt1 = wordArray.join(' ');
+			const textArray = [...text];
+			block.innerHTML = txt1;
+			while (block.scrollHeight - block.offsetHeight > 1) {
+				textArray.pop();
+				txt1 = textArray.join('');
+				block.innerHTML = txt1;
+			}
+			const gra = textArray.splice(textArray.length - 5, 5).map((s, i, a) => `<span style='opacity: 0.${1 + (100 * (8 * (1 - (i / (a.length - 1)))))};'>${s}</span>`);
+			txt1 = textArray.join('') + gra.join('');
+			block.innerHTML = txt1;
+		}
+	};
+
+	componentDidMount() {
+		window.addEventListener('resize',this.myResize);
+		this.myResize();
+	};
+
+	render() {
+		let {font} = this.props;
+		return (
+			<div>
+				<div>{font}</div>
+				<div><span style={{fontFamily:font, border: "1px solid gray"}}>font Ie test</span></div>
+				<div style={{fontFamily:font, lineHeight:'1em',maxHeight:'1em',wordBreak:'break-all'}} ref={this.ref} className={"Block_Opacity"}></div>
+				<hr/><br/>
+			</div>
+		);
+
+	}
+
+}
+
+
+
+export default OpacityAndBlockTest;
