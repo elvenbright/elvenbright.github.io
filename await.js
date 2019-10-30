8a50b6c5d143e8800309c5c1f1ac4043e0013e76
danilads <vfhn3fghtkm>
2019-10-30 13:54:54 +0300

    up

--- await.js
+++ await.js
@@ -1,4 +1,27 @@
-//request by url
+let answerArr = []; 
+
+const startAwait=async(url)=>{
+    let answer = await getInfAwait(url);
+    
+    //если ответ json
+    if(answer.length>3 && answer[0]==="[" && answer[answer.length-1] === "]"){
+        let answArr = new Promise((resolve, reject) => {
+            resolve(сorrelation(JSON.parse(answer)));
+        })
+        answArr.then((value)=>{
+            console.log('---value',value)
+        })
+        console.log('---wtf in answArr',answArr);
+        answerArr.push(...answArr);
+        console.log('---wtf in answerArr',answerArr);
+    }
+    //если ответ строка
+    else if(typeof answer === 'string'){
+        answerArr.push(answer);
+    }
+    
+}
+
 const getInfAwait = async (url) => {
     try{
         let answer = await fetch("https://fe.it-academy.by/Examples/words_tree/"+url, {
@@ -6,30 +29,25 @@ const getInfAwait = async (url) => {
         });
         let text = await answer.text();
 
-        responseHandler(text);
-        
+        return text;
     }
     catch(err){
         console.error('---error request',err);
     }
 };
 
-let answerArr = [];
 
-const responseHandler = async (answer) => {
-   
-    if(answer.length>3 && answer[0]==="[" && answer[answer.length-1] === "]"){
-        let toArr = JSON.parse(answer);
 
-        
-        for(let i=0;toArr.length>i;i++) {
-            await getInfAwait(toArr[i]);
-        }
-            
-        
-    }
-    else if(typeof answer === 'string'){
-        answerArr.push(answer);
-    }
-
+//получает массив - возвращает массив ответов
+const сorrelation = async (array) => {
+    console.log('---сorrelation in',array);
+   let resultArr = [];
+   for(let i=0;array.length>i;i++){
+        let answer = await getInfAwait(array[i]);
+        resultArr.push(answer);
+   }
+   console.log('---сorrelation out',resultArr);
+   return resultArr;
 }
+
+
